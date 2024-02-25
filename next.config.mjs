8e0686672a234591f632474cae90acb3185ebac2
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-02-25 18:43:32 -0500

    run export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
       remotePatterns: [
         {
